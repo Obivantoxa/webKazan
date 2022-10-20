2377c8596c7a5fa86f036542d053341ea6b022af
Obivantoxa <Obivan.toxa@yandex.ru>
2022-10-20 18:32:28 +0300

    add new controller many fixes

---
+++ src/main/java/constants/Constat.java
@@ -0,0 +1,12 @@
+package constants;
+
+public interface Constat {
+
+
+    String CONNECTION_URL_DB = "jdbc:mysql://localhost:3306/students3";
+    String CONNECTION_LOGIN_DB = "root";
+    //local
+    //String CONNECTION_PASSWORD_DB = "admin";
+    //host
+    String CONNECTION_PASSWORD_DB = "admin123";
+}
